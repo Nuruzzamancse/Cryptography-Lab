9981192fe081dafbc299044786f686a5ec506b8a
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2018-04-02 08:23:42 +0600

    OneTimePad Edited

--- OnetimePad.cpp
+++ OnetimePad.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main(){
 
-    char msg[10000], pad[10000], identical[10000], encriptedmsg[10000], i=0, onti, mgi;
+    char msg[10000], pad[10000],encriptedmsg[10000], i=0, onti, mgi;
 
-    queue<char> onetimepad;
+    queue<char> onetimepad,identicalOntimepad;
 
     cout<<"Enter Your message"<<endl;
     gets(msg);
@@ -14,16 +14,18 @@ int main(){
     cout<<"Enter Onetime Pad"<<endl;
     gets(pad);
 
-    strcpy(identical,pad);
 
-    for(i=0;pad[i];i++)
+    for(i=0;pad[i];i++){
         onetimepad.push(pad[i]);
+        identicalOntimepad.push(pad[i]);
+        }
 
     for(i=0;msg[i];i++){
 
         char ont = onetimepad.front();
 
-        onetimepad.pop();
+        if(!onetimepad.empty())
+            onetimepad.pop();
 
         if(ont>='a'&&ont<='z')
              onti = ont - 96;
@@ -69,18 +71,24 @@ int main(){
 
     for(int i=0;encriptedmsg[i];i++)
     {
+        char id = identicalOntimepad.front();
+                    if(!identicalOntimepad.empty())
+                        identicalOntimepad.pop();
 
     if(encriptedmsg[i]>='a'&&encriptedmsg[i]<='z')
-            if((encriptedmsg[i]-identical[i])<=0)
-                printf("%c",encriptedmsg[i]-identical[i]+26+96);
+            if((encriptedmsg[i]-id)<=0){
+
+                        printf("%c",encriptedmsg[i]-id+26+96);
+
+            }
             else
-                printf("%c",encriptedmsg[i]-identical[i]+96);
+                printf("%c",encriptedmsg[i]-id+96);
     else if(encriptedmsg[i]>='A'&&encriptedmsg[i]<='Z')
 
-            if((encriptedmsg[i]-identical[i])<=0)
-                printf("%c",encriptedmsg[i]-identical[i]+26+64);
+            if((encriptedmsg[i]-id)<=0)
+                printf("%c",encriptedmsg[i]-id+26+64);
             else
-                printf("%c",encriptedmsg[i]-identical[i]+64);
+                printf("%c",encriptedmsg[i]-id+64);
 
 
     }
